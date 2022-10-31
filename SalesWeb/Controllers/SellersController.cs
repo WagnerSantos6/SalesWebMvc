dba0205cd4e7b4ff7658c9c0314fdd57ccc15370
Wagner Santos <wagnerstss@gmail.com>
2022-10-31 15:32:41 -0300

    Created SellersController

---
+++ SalesWeb/Controllers/SellersController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SalesWeb.Controllers
+{
+    public class SellersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
