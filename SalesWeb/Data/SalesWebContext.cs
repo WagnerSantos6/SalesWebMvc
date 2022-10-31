b8b730045894286ca8b077a1d4079f5ec125c4f0
Wagner Santos <wagnerstss@gmail.com>
2022-10-31 10:55:35 -0300

    More entities and second migration

--- SalesWeb/Data/SalesWebContext.cs
+++ SalesWeb/Data/SalesWebContext.cs
@@ -14,6 +14,8 @@ namespace SalesWeb.Data
         {
         }
 
-        public DbSet<SalesWeb.Models.Department> Department { get; set; }
+        public DbSet<Department> Department { get; set; }
+        public DbSet<Seller> Seller { get; set; }
+        public DbSet<SalesRecord> SalesRecord { get; set; }
     }
 }
--- SalesWeb/Models/Department.cs
+++ SalesWeb/Models/Department.cs
@@ -1,8 +1,34 @@
-﻿namespace SalesWeb.Models
+﻿using System.Collections.Generic;
+using System;
+using System.Linq;
+
+namespace SalesWeb.Models
 {
     public class Department
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public ICollection<Seller> Sellers { get; set; } = new List<Seller>();
+
+        public Department()
+        {
+           
+        }
+
+        public Department(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public void AddSeller(Seller seller)
+        {
+            Sellers.Add(seller);
+        }
+
+        public double TotalSales(DateTime initial, DateTime final)
+        {
+            return Sellers.Sum(seller => seller.TotalSales(initial, final));
+        }
     }
 }
