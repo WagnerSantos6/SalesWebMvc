912159537ee18a70ec450748cd2fbcdd42e4a011
Wagner Santos <wagnerstss@gmail.com>
2022-11-04 14:26:20 -0300

    Validation

--- SalesWeb/Controllers/SellersController.cs
+++ SalesWeb/Controllers/SellersController.cs
@@ -35,6 +35,12 @@ namespace SalesWeb.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Create(Seller seller)
         {
+            if(!ModelState.IsValid)
+            {
+                var departments = _departmentService.FindAll();
+                var viewModel = new SellerFormViewModel { Seller = seller, Departments = departments };
+                return View(viewModel);
+            }
             _sellerService.Insert(seller);
             return RedirectToAction(nameof(Index));
         }
@@ -98,6 +104,13 @@ namespace SalesWeb.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Edit(int id, Seller seller)
         {
+            if (!ModelState.IsValid)
+            {
+                var departments = _departmentService.FindAll();
+                var viewModel = new SellerFormViewModel { Seller = seller, Departments = departments };
+                return View(viewModel);
+            }
+
             if (id != seller.Id)
             {
                 return RedirectToAction(nameof(Error), new { message = "Id mismatch" });
--- SalesWeb/Models/Seller.cs
+++ SalesWeb/Models/Seller.cs
@@ -8,19 +8,29 @@ namespace SalesWeb.Models
     public class Seller
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "{0} riquered")]
+        [StringLength(60, MinimumLength = 3, ErrorMessage = "{0} size minimum {2} and maximum {1}")]
         public string Name { get; set; }
 
+        [Required(ErrorMessage = "{0} riquered")]
+        [EmailAddress(ErrorMessage = "Incorrect email format")]
         [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
 
+        [Required(ErrorMessage = "{0} riquered")]
         [Display(Name = "Birth Date")]
         [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         [DataType(DataType.Date)]
         public DateTime BirthDate { get; set; }
 
+        [Required(ErrorMessage = "{0} riquered")]
+        [Range(100.0, 50000.0, ErrorMessage = "{0} must be between {1} and {2}")]
         [Display(Name = "Base Salary")]
         [DisplayFormat(DataFormatString = "{0:F2}")]
         public double BaseSalary { get; set; }
+
+
         public Department Department { get; set; }
         public int DepartmentId { get; set; }
         public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
@@ -42,7 +52,7 @@ namespace SalesWeb.Models
 
         public void AddSales(SalesRecord sr)
         {
-            Sales.Add(sr);  
+            Sales.Add(sr);
         }
 
         public void RemoveSales(SalesRecord sr)
