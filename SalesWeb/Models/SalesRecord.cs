d7696ac3192cbdfa8b4b5363c096e02cdf9e8da6
Wagner Santos <wagnerstss@gmail.com>
2022-11-14 18:11:52 -0300

    fix amount groupingSearch

--- SalesWeb/Models/SalesRecord.cs
+++ SalesWeb/Models/SalesRecord.cs
@@ -12,7 +12,7 @@ namespace SalesWeb.Models
         [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime Date { get; set; }
 
-        [DisplayFormat(DataFormatString = "{0: F2}")]
+        [DisplayFormat(DataFormatString = "{0:F2}")]
         public double Amount { get; set; }
         public SaleStatus Status { get; set; }
         public Seller Seller { get; set; }
